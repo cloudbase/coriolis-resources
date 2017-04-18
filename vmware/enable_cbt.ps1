40c519b93c0c22c6ba6b74f977e9753c68092e31
Alessandro Pilotti <apilotti@cloudbasesolutions.com>
2017-04-18 15:16:53 +0300

    Improves enable_cbt.ps1

--- vmware/enable_cbt.ps1
+++ vmware/enable_cbt.ps1
@@ -1,16 +1,31 @@
 Param(
   [string]$Server,
   [string]$VMName,
-  [PSCredential]$Credential
+  [PSCredential]$Credential,
+  [switch]$Enabled = $true
 )
 
 $ErrorActionPreference = "Stop"
 
 Import-Module VMware.VimAutomation.Core
 
-Connect-VIServer -Server $server -Credential $Credential
+Connect-VIServer -Server $server -Credential $Credential | Out-Null
 
-$vm = Get-vm $VMName | Get-View
-$vmConfigSpec = New-Object VMware.Vim.VirtualMachineConfigSpec
-$vmConfigSpec.changeTrackingEnabled = $true
-$vm.reconfigVM($vmConfigSpec)
+$vm = Get-vm $VMName
+$vmView = $vm | Get-View
+
+if ($vmView.Config.changeTrackingEnabled -eq $Enabled)
+{
+    Write-Verbose "CBT is already set to $Enabled for this VM" -Verbose
+}
+else
+{
+    if ($vm | Get-Snapshot)
+    {
+        throw "Remove all VM snapshots before changing the CBT setting"
+    }
+
+    $vmConfigSpec = New-Object VMware.Vim.VirtualMachineConfigSpec
+    $vmConfigSpec.changeTrackingEnabled = $Enabled
+    $vmView.reconfigVM($vmConfigSpec)
+}
